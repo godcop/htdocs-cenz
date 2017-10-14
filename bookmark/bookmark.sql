30aa5236bb3500c860a4679350e4eec95e89e04c
godcop <godcoper@gmail.com>
2017-10-14 22:17:32 +0800

    跟上一个commit一样，唯独更新了正确的数据库导出的文件

--- bookmark/bookmark.sql
+++ bookmark/bookmark.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: 2017-10-14 07:32:45
+-- Generation Time: 2017-10-14 16:15:36
 -- 服务器版本： 10.1.26-MariaDB
 -- PHP Version: 7.1.9
 
@@ -32,8 +32,21 @@ CREATE TABLE `user` (
   `uid` int(11) NOT NULL,
   `username` varchar(30) DEFAULT NULL,
   `password` varchar(30) DEFAULT NULL,
-  `email` varchar(30) DEFAULT NULL
+  `email` varchar(30) DEFAULT NULL,
+  `title1` text,
+  `title2` text,
+  `bgurl` varchar(100) DEFAULT NULL,
+  `links` varchar(300) DEFAULT NULL,
+  `search` varchar(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- 转存表中的数据 `user`
+--
+
+INSERT INTO `user` (`uid`, `username`, `password`, `email`, `title1`, `title2`, `bgurl`, `links`, `search`) VALUES
+(6, 'Cenz', 'cenz', '1@qq.com', 'ä½ å¥½', '', '', '', 'baidu'),
+(7, 'æ©™å­', 'cenz', '2@qq.com', '', '', 'http://192.168.31.124/1.jpg', '', 'baidu');
 
 --
 -- Indexes for dumped tables
@@ -43,7 +56,7 @@ CREATE TABLE `user` (
 -- Indexes for table `user`
 --
 ALTER TABLE `user`
-  ADD UNIQUE KEY `id` (`uid`);
+  ADD UNIQUE KEY `uid` (`uid`) USING BTREE;
 
 --
 -- 在导出的表使用AUTO_INCREMENT
@@ -53,7 +66,7 @@ ALTER TABLE `user`
 -- 使用表AUTO_INCREMENT `user`
 --
 ALTER TABLE `user`
-  MODIFY `uid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `uid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
